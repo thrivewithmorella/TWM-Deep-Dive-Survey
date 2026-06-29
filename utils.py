f2cc71f7cc35d95b4cb92633dd4abdb80f939c83
Vicky Coll <victoriacollg@googlemail.com>
2026-06-28 19:44:01 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,26 +2,22 @@ import gspread
 from google.oauth2.service_account import Credentials
 from datetime import datetime
 import streamlit as st
+import json
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
 def save_response(responses, name, phone, email, circle):
     try:
-        # Get credentials from Streamlit secrets
-        client_id = st.secrets["client_id"]
-        client_secret = st.secrets["client_secret"]
+        # Get service account credentials from Streamlit secrets
+        service_account_info = st.secrets["google_service_account"]
         
-        # Create credentials manually
-        credentials_dict = {
-            "type": "oauth2",
-            "client_id": client_id,
-            "client_secret": client_secret,
-            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-            "token_uri": "https://oauth2.googleapis.com/token",
-        }
+        # Create credentials from the service account info
+        credentials = Credentials.from_service_account_info(
+            service_account_info,
+            scopes=SCOPES
+        )
         
-        # Authenticate with Google Sheets
-        credentials = Credentials.from_authorized_user_info(credentials_dict, scopes=SCOPES)
+        # Authorize with gspread
         client = gspread.authorize(credentials)
         
         # Open the Google Sheet by name
